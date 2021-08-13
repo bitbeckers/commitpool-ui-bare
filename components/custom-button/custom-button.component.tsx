10dbd30558afe41f9e51fc2adafd5322b82c55ec
bitbeckers <code@bitbeckers.com>
2021-08-13 15:13:26 +0200

    added aria interactions, updated Button component to support hover and update css

--- components/custom-button/custom-button.component.tsx
+++ components/custom-button/custom-button.component.tsx
@@ -1,15 +1,30 @@
-import React from "react";
-import { StyleSheet, Text, Platform, Pressable, StyleProp, ViewStyle } from "react-native";
+import React, { useRef } from "react";
+import {
+  StyleSheet,
+  Text,
+  Platform,
+  Pressable,
+  StyleProp,
+  ViewStyle,
+} from "react-native";
+import { useHover } from "@react-native-aria/interactions";
 
 interface CustomButtonProps {
-  text: string,
-  onPress: () => any,
+  text: string;
+  onPress: () => any;
   style?: StyleProp<ViewStyle>;
 }
 
 const CustomButton = ({ text, onPress, style }: CustomButtonProps) => {
+  const ref = useRef(null);
+  const { isHovered, hoverProps } = useHover({}, ref);
+
   return (
-    <Pressable onPress={onPress} style={[styles.button, style]}>
+    <Pressable
+      ref={ref}
+      onPress={onPress}
+      style={[styles.button, isHovered && styles.hovered, style]}
+    >
       <Text style={styles.text}>{text}</Text>
     </Pressable>
   );
@@ -18,19 +33,24 @@ const CustomButton = ({ text, onPress, style }: CustomButtonProps) => {
 const styles = StyleSheet.create({
   button: {
     flex: 1,
+    cursor: "pointer",
     alignSelf: "center",
     justifyContent: "center",
     alignItems: "center",
     width: 250,
     maxWidth: 300,
-    maxHeight: 42,
+    maxHeight: "3em",
     backgroundColor: "rgba(212, 84, 84, 1)",
     margin: 10,
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingRight: 10,
-    paddingLeft: 10,
+    padding: 10,
     borderRadius: 12,
+  },
+  text: {
+    color: "white",
+    fontFamily: "OpenSans_400Regular",
+  },
+  hovered: {
+    backgroundColor: "#C43131",
     ...Platform.select({
       ios: {
         shadowColor: "rgba(0,0,0, .7)",
@@ -45,11 +65,6 @@ const styles = StyleSheet.create({
         boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
       },
     }),
-
-  },
-  text: {
-    color: "white",
-    fontFamily: 'OpenSans_400Regular',
   },
 });
 
